0740a5520e14250dfe7d139a5d0fc3e766cd02bb
hidekazuyamamoto <b.r.j.hide@gmail.com>
2021-10-23 10:17:23 +0900

    調整

--- app/controllers/timer_count_controller.rb
+++ app/controllers/timer_count_controller.rb
@@ -2,6 +2,8 @@ class TimerCountController < ApplicationController
   def new
     @timer_title = TimerTitle.find(params[:timer_title_id])
     @timer_count = TimeCount.new
+    @history = TimeCount.where(timer_title_id: params[:timer_title_id])
+    time_calc(@history)
   end
   def create
     @timer_title = TimerTitle.find(params[:timer_title_id])
@@ -12,5 +14,17 @@ class TimerCountController < ApplicationController
   private
   def params_timer_count
     params.permit(:time).merge(timer_title_id: @timer_title.id)
+  end
+  def time_calc(history)
+    @history_time = []
+    history.each do |history|
+      hour = (history.time/(60*60)).floor.to_s
+      history.time %= 60*60
+      min = ("0" + (history.time/(60)).floor.to_s).slice(-2,2)
+      history.time %= 60
+      sec = ("0" + history.time.floor.to_s).slice(-2,2)
+      timer = hour +":"+min+":"+sec
+      @history_time.push(timer)
+    end
   end
 end
